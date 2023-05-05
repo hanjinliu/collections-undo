0a0e6cad69f353e51dd4840e4443aaf5dba87f54
Hanjin Liu <hanjin.liu@bs.s.u-tokyo.ac.jp>
2023-05-05 09:14:25 +0900

    use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup, find_packages
+import sys
 
-with open("collections_undo/__init__.py", encoding="utf-8") as f:
-    line = next(iter(f))
-    VERSION = line.strip().split()[-1][1:-1]
+sys.stderr.write(
+    """
+    =======================================================================
+    collections-undo does not support `python setup.py install`. Please use
 
-with open("README.md") as f:
-    readme = f.read()
+        $ python -m pip install .
+
+    instead.
+    =======================================================================
+    """
+)
+sys.exit(1)
 
 setup(
     name="collections-undo",
-    version=VERSION,
     description="General undo/redo framework for Python",
-    long_description=readme,
     long_description_content_type="text/markdown",
     author="Hanjin Liu",
     author_email="liuhanjin-sc@g.ecc.u-tokyo.ac.jp",
     license="MIT",
     download_url="https://github.com/hanjinliu/collections-undo",
-    packages=find_packages(exclude=["docs", "examples", "rst", "tests", "tests.*"]),
-    package_data={"undo": ["**/*.pyi", "*.pyi"]},
     install_requires=[
         "typing_extensions",
     ],
